5790ccc142de4d067795c2eb39db763cffa0991b
kuzhanthaivel <kuzhanthaivel272@gmail.com>
2024-04-06 23:07:10 +0530

    site

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router,Routes,Route,} from "react-router-dom";
 import Main from './pages/Main';
 import Verifypage from "./pages/Verifypage";
-
-
-
+import SearchBar from "./component/SearchBar.jsx"
 function App() {
   return (
     <>  
-    
+    {/* <video className="top-1 z-0" src={Videobg2} autoPlay loop muted/>
         <Routes>
 
         <Route path="/" element={<Main />}></Route>
         <Route path="verifykyc3" element={<Verifypage />}></Route>
         </Routes>
+
+      */}
+      <SearchBar/>
       
     </>
   );
